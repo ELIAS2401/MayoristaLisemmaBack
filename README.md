794af727f38510e8c32e59bc1eb72dee411d57d7
ELIAS2401 <tuccieliasluis@gmail.com>
2026-01-15 14:38:10 -0300

    producto back

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MayoristaLisemmaBack
 Backend de aplicación para mayorista de bebidas
+//npx prisma migrate dev --name nombre_cambio_update
--- src/repository/producto.repository.ts
+++ src/repository/producto.repository.ts
@@ -11,13 +11,13 @@ export class ProductoRepository {
     }
 
     async obtenerProductoPorId(productoId: number) {
-    return prisma.producto.findUnique({
-        where: { id: productoId },
-        include: {
-            categoria: true
-        }
-    });
-}
+        return prisma.producto.findUnique({
+            where: { id: productoId },
+            include: {
+                categoria: true
+            }
+        });
+    }
 
     async crearProducto(data: any) {
         return prisma.producto.create({
@@ -26,8 +26,9 @@ export class ProductoRepository {
     }
 
     async eliminarProducto(productoId: number) {
-        return prisma.producto.delete({
-            where: { id: productoId }
+        return prisma.producto.update({
+            where: { id: productoId },
+            data: { activo: false }
         });
     }
 
